1ee6ddb766e35198280e33ebdd6f7fbe2e36553f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-30 11:52:47 +0300

    Updated Sampling  and Math dependencies.

--- Package.swift
+++ Package.swift
@@ -13,16 +13,15 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "DataStructure", url: "https://github.com/StarlangSoftware/DataStructure-Swift.git", .exact("1.0.4")),
-        .package(name: "Math", url: "https://github.com/StarlangSoftware/Math-Swift.git", .exact("1.0.10")),
-        .package(name: "Sampling", url: "https://github.com/StarlangSoftware/Sampling-Swift.git", .exact("1.0.5")),
-        .package(name: "Util", url: "https://github.com/StarlangSoftware/Util-Swift.git", .exact("1.0.6"))
+        .package(name: "Math", url: "https://github.com/StarlangSoftware/Math-Swift.git", .exact("1.0.11")),
+        .package(name: "Sampling", url: "https://github.com/StarlangSoftware/Sampling-Swift.git", .exact("1.0.6")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Classification",
-            dependencies: ["DataStructure", "Math", "Sampling", "Util"]),
+            dependencies: ["DataStructure", "Math", "Sampling"]),
         .testTarget(
             name: "ClassificationTests",
             dependencies: ["Classification"]),
